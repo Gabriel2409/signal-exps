be70313863dcc1b5bf0f3c8b8816c9ed3352deb4
Gabriel2409 <gabriel.cournelle@gmail.com>
2026-05-08 10:28:19 +0200

    custom validator

--- src/app/myform/myform.ts
+++ src/app/myform/myform.ts
@@ -9,11 +9,14 @@ import {
   minLength,
   pattern,
   required,
+  validate,
 } from '@angular/forms/signals';
 
 interface SignupForm {
   username: string;
-  email: '';
+  email: string;
+  password: string;
+  confirmPassword: string;
   age: number | null;
 }
 
@@ -27,16 +30,37 @@ export class Myform {
   protected myformModel = signal<SignupForm>({
     username: '',
     email: '',
+    password: '',
+    confirmPassword: '',
     age: null,
   });
 
   protected form = form(this.myformModel, (s) => {
-    (required(s.username, { message: 'username is required' }),
-      required(s.email, { message: 'Email is required' }),
-      minLength(s.username, 3, { message: 'Username must be at least 3 chars' }),
-      email(s.email, { message: 'Please enter a valid email' }),
-      pattern(s.username, /^[a-zA-Z0-9]+$/, { message: 'Must contain only letters and numbers' }),
-      min(s.age, 13, { message: 'You must be 13 or more' }),
-      max(s.age, 120, { message: 'You must be 120 or less' }));
+    required(s.username, { message: 'username is required' });
+    minLength(s.username, 3, { message: 'Username must be at least 3 chars' });
+    pattern(s.username, /^[a-zA-Z0-9]+$/, { message: 'Must contain only letters and numbers' });
+    required(s.email, { message: 'Email is required' });
+    email(s.email, { message: 'Please enter a valid email' });
+    min(s.age, 13, { message: 'You must be 13 or more' });
+    max(s.age, 120, { message: 'You must be 120 or less' });
+    required(s.password, { message: 'Please enter a password' });
+    // custom validator
+    validate(s.password, ({ value }) => {
+      const password = value();
+      if (!password) {
+        // let required validator handle it
+        return null;
+      }
+      if (password.includes(' ')) {
+        return {
+          // returns the error type
+          kind: 'no_space',
+          // error message
+          message: 'Your password cannot contain spaces',
+        };
+      }
+      // no errors
+      return null;
+    });
   });
 }
