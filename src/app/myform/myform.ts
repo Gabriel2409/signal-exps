3097843ed9e5b7e4dc2e5b7cfa27f718efe1bf51
Gabriel2409 <gabriel.cournelle@gmail.com>
2026-05-08 13:45:13 +0200

    async validator

--- src/app/myform/myform.ts
+++ src/app/myform/myform.ts
@@ -1,5 +1,5 @@
 import { JsonPipe } from '@angular/common';
-import { Component, signal } from '@angular/core';
+import { Component, resource, signal } from '@angular/core';
 import {
   email,
   form,
@@ -10,6 +10,7 @@ import {
   pattern,
   required,
   validate,
+  validateAsync,
 } from '@angular/forms/signals';
 
 interface SignupForm {
@@ -82,5 +83,56 @@ export class Myform {
       // no errors
       return null;
     });
+
+    // async validator, exposes a pending signal on the field
+    validateAsync(s.username, {
+      params: ({ value }) => {
+        const val = value();
+        if (!val || val.length < 3) {
+          // undefined tells signal forms not to run the async validator right now
+          return undefined;
+        }
+        // value for the next step
+        return val;
+      },
+      factory: (params) =>
+        // creates a resource from the params that tracks the async operation
+        resource({
+          params,
+          loader: async ({ params }) => {
+            const username = params;
+            const available = await this.checkUsernameAvailability(username);
+            return available;
+          },
+        }),
+      // what happens if the async validator does not throw an error
+      onSuccess: (result: boolean) => {
+        if (result === false) {
+          return {
+            kind: 'username_taken',
+            message: 'This username is already taken',
+          };
+          // no async errors
+        }
+        return null;
+      },
+      // what happens if the async validator throws an error, for ex if there is a
+      // network problem
+      onError: (error: unknown) => {
+        console.log('Validation error', error);
+        // here we dont block the form in case of unexpected errrors but in real app,
+        // we probably should
+        return null;
+      },
+    });
   });
+
+  private checkUsernameAvailability(username: string): Promise<boolean> {
+    return new Promise((resolve) => {
+      setTimeout(() => {
+        const taken = ['admin', 'test', 'brian'];
+        resolve(!taken.includes(username.toLowerCase()));
+      }, 1000);
+    });
+  }
 }
