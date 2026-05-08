011c15db6f6e7ff32638e7aa5092ca63a6933ed9
Gabriel2409 <gabriel.cournelle@gmail.com>
2026-05-08 10:17:12 +0200

    built in validators

--- src/app/myform/myform.ts
+++ src/app/myform/myform.ts
@@ -1,27 +1,42 @@
 import { JsonPipe } from '@angular/common';
 import { Component, signal } from '@angular/core';
-import { form, FormField, FormRoot } from '@angular/forms/signals';
+import {
+  email,
+  form,
+  FormField,
+  max,
+  min,
+  minLength,
+  pattern,
+  required,
+} from '@angular/forms/signals';
 
 interface SignupForm {
   username: string;
+  email: '';
+  age: number | null;
 }
 
 @Component({
   selector: 'app-myform',
-  imports: [FormField, FormRoot, JsonPipe],
+  imports: [FormField, JsonPipe],
   templateUrl: './myform.html',
   styleUrl: './myform.scss',
 })
 export class Myform {
   protected myformModel = signal<SignupForm>({
     username: '',
+    email: '',
+    age: null,
   });
 
-  protected form = form(this.myformModel, (s) => {}, {
-    submission: {
-      action: async (f) => {
-        this.myformModel.set({ username: 'aaa' });
-      },
-    },
+  protected form = form(this.myformModel, (s) => {
+    (required(s.username, { message: 'username is required' }),
+      required(s.email, { message: 'Email is required' }),
+      minLength(s.username, 3, { message: 'Username must be at least 3 chars' }),
+      email(s.email, { message: 'Please enter a valid email' }),
+      pattern(s.username, /^[a-zA-Z0-9]+$/, { message: 'Must contain only letters and numbers' }),
+      min(s.age, 13, { message: 'You must be 13 or more' }),
+      max(s.age, 120, { message: 'You must be 120 or less' }));
   });
 }
